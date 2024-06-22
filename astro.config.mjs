cc80dbb8b5d7d126e85f9aac030c1e934c4bac5c
Thomas Green <25429407+thomasgreen@users.noreply.github.com>
2024-06-22 20:59:49 +0100

    Homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
+// https://astro.build/config
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), tailwind(), icon()]
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue}',
+    './public/**/*.html',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
