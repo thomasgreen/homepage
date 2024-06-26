e130bf3494b456c08b8ce131ab5c4bb046d10778
Thomas Green <25429407+thomasgreen@users.noreply.github.com>
2024-06-26 15:08:29 +0100

    add prettier rc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  htmlWhitespaceSensitivity: "ignore",
+  trailingComma: "none",
+  endOfLine: "lf",
+  singleAttributePerLine: true,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  vueIndentScriptAndStyle: false,
+};
